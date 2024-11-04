b768b8c01f0a44cbc04e2b45248fa2581d0e8360
HouckDev <houckdeveloper@gmail.com>
2024-11-04 16:29:51 -0600

    Basic empty UI frame

--- src/main/java/edu/grinnell/csc207/UserInterface.java
+++ src/main/java/edu/grinnell/csc207/UserInterface.java
@@ -1,9 +1,31 @@
 package edu.grinnell.csc207;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.*;
+
 public class UserInterface {
   App owningApp;
+  UIPanel mainPanel;
   public UserInterface(App newOwningApp) {
     this.owningApp = newOwningApp;
+    
+    JFrame frame = new JFrame("Space Sim");
+    frame.getContentPane().setLayout(new BorderLayout());
+    mainPanel = new UIPanel();
+    
+    frame.getContentPane().add(mainPanel);
+    
+    frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+    //frame.setSize(300,300);
+    frame.setUndecorated(true);
+    frame.setVisible(true);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
   } // UserInterface(App)
+
+  public class UIPanel extends JPanel {
+
+  }
 } // UserInterface
 
