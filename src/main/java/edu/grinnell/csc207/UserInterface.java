5b9aad925d8f243d15dc1f0b5db97832817490f0
HouckDev <houckdeveloper@gmail.com>
2024-11-05 09:22:10 -0600

    Template command prompt ui

--- src/main/java/edu/grinnell/csc207/UserInterface.java
+++ src/main/java/edu/grinnell/csc207/UserInterface.java
@@ -1,7 +1,9 @@
 package edu.grinnell.csc207;
 
+import javax.swing.Box;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import java.awt.*;
 
 public class UserInterface {
@@ -13,19 +15,23 @@ public class UserInterface {
     JFrame frame = new JFrame("AbstractGame");
     frame.getContentPane().setLayout(new BorderLayout());
     mainPanel = new UIPanel();
-    
     frame.getContentPane().add(mainPanel);
-    
-    frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-    //frame.setSize(300,300);
-    frame.setUndecorated(true);
+    //frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+    frame.setSize(300,300);
+    //frame.setUndecorated(true);
     frame.setVisible(true);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     
   } // UserInterface(App)
 
   public class UIPanel extends JPanel {
+    public UIPanel() {
+      Box horizontalBox = Box.createHorizontalBox();
+      horizontalBox.add(new Label(">:"));
+      horizontalBox.add(new JTextField(20));
 
+      add(horizontalBox);
+    }
   }
 } // UserInterface
 
