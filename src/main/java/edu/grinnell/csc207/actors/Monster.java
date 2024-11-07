aa3429ddccd376c939cd09343d11a585da0de743
mitchydev <paivamit@grinnell.edu>
2024-11-07 14:12:40 -0600

    Monster in office check

--- src/main/java/edu/grinnell/csc207/actors/Monster.java
+++ src/main/java/edu/grinnell/csc207/actors/Monster.java
@@ -3,6 +3,7 @@ package edu.grinnell.csc207.actors;
 import edu.grinnell.csc207.Level;
 import edu.grinnell.csc207.matrix.MatrixV0;
 import edu.grinnell.csc207.rooms.Hallway;
+import edu.grinnell.csc207.rooms.OfficeRoom;
 import edu.grinnell.csc207.rooms.Room;
 
 /**
@@ -16,7 +17,7 @@ public class Monster extends Actor {
   // create a new monster
   public Monster(Level newLevel) {
     super(newLevel);
-  }
+  } // monster
 
   /**
    * Advance the monster's movement to a valid room without a closed door.
@@ -26,6 +27,9 @@ public class Monster extends Actor {
   public void advanceTurn() {
     int[][] directions = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
     int direction = (int) (Math.random() * directions.length);
+    if (getCurrentRoom() instanceof OfficeRoom) {
+      // STUB
+    }
     while (getOwningLevel().getLevelRooms().get(getPosition()[0] + directions[direction][0],
         getPosition()[1] + directions[direction][1]) == null) {
       direction = (int) (Math.random() * directions.length);
