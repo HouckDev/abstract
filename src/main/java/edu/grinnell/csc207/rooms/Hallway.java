83f7f98752169e3e5943ded9629dfa29e32436b2
mitchydev <paivamit@grinnell.edu>
2024-11-07 14:38:48 -0600

    Added Door Actor

--- src/main/java/edu/grinnell/csc207/rooms/Hallway.java
+++ src/main/java/edu/grinnell/csc207/rooms/Hallway.java
@@ -3,12 +3,16 @@ package edu.grinnell.csc207.rooms;
 import javax.swing.Box;
 import javax.swing.JComponent;
 import edu.grinnell.csc207.Level;
+import edu.grinnell.csc207.actors.Player;
 import edu.grinnell.csc207.userInterface.TerminalLabel;
+import edu.grinnell.csc207.actors.Door;
 
 public class Hallway extends Room {
   public Hallway(Level newLevel) {
     super(newLevel);
+    addActor(new Door(newLevel));
   } // Hallway
+  
   @Override
   public JComponent constructMapWidget() {
     Box roomWidget = Box.createVerticalBox();
