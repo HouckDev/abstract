231648c6b9c091c3d5c5d4fc8fd300a75126f42c
HouckDev <houckdeveloper@gmail.com>
2024-11-07 10:37:41 -0600

    Console log implementation

--- src/main/java/edu/grinnell/csc207/userInterface/TerminalWidget.java
+++ src/main/java/edu/grinnell/csc207/userInterface/TerminalWidget.java
@@ -8,14 +8,18 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import edu.grinnell.csc207.userInterface.UserInterface.UIPanel;
 
 public class TerminalWidget extends Widget {
   Box consoleLog;
+
   public TerminalWidget(UserInterface owningUserInterface) {
     super(owningUserInterface);
     setTitle("Terminal");
@@ -32,33 +36,42 @@ public class TerminalWidget extends Widget {
     commandField.setBackground(new Color(0, 0, 0));
     horizontalBox.add(commandField);
 
+    // construct confirm button
     JButton confirmButton = new JButton("Commit");
     confirmButton.setForeground(new Color(64, 255, 128));
-
-    //Test button input, advance turn on commit pressed
-    confirmButton.addActionListener(new ActionListener() {
-          @Override
-          public void actionPerformed(ActionEvent e) {
-            getUserInterface().owningApp.getCurrentGame().advanceTurn();
-          }
-      });
     confirmButton.setBackground(new Color(0, 0, 0));
 
     horizontalBox.add(confirmButton);
     // construct console log
     consoleLog = Box.createVerticalBox();
+    for (int i = 0; i < 9; i++) {
+      consoleLog.add(new TerminalLabel(""));
+    }
     consoleLog.add(new TerminalLabel("> Game Begin"));
     consoleLog.setBackground(new Color(8, 32, 16));
     JScrollPane scrollPane = new JScrollPane(consoleLog);
-    
     // construct main ui
     Box mainBox = Box.createVerticalBox();
     mainBox.add(scrollPane);
     mainBox.add(horizontalBox);
 
     add(mainBox);
-    
-    setSize(300,300);
+
+    // add input
+
+    // Test button input, advance turn on commit pressed
+    confirmButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        consoleLog.add(new TerminalLabel("> " + commandField.getText()));
+        JScrollBar vertical = scrollPane.getVerticalScrollBar();
+        validate();
+        vertical.setValue(vertical.getMaximum());
+        getUserInterface().owningApp.getCurrentGame().advanceTurn();
+      }
+    });
+
+    setSize(300, 300);
     setVisible(true);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
   }
